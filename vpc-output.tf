f7a789321208bb6be7efc80742e6d0dd781b5686
csk17k <84774760+csk17k@users.noreply.github.com>
2021-06-17 12:38:17 +0530

    Create vpc-output.tf

---
+++ vpc-output.tf
@@ -0,0 +1,8 @@
+output "vpc-name" {
+  # value = var.enable_autoscaling ? "${google_compute_instance_template.webserver[0].self_link}" : ""
+  value = google_compute_network.xcloud-vpc.name
+}
+output "subnet-self_link" {
+  # value = var.enable_autoscaling ? "${google_compute_instance_template.webserver[0].self_link}" : ""
+  value = google_compute_subnetwork.xcloud-subnet.self_link
+}
